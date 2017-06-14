77a137a36095d78824d219282c681b327d581e1d
Mark Zeagler <mark.zeagler@outlook.com>
2017-06-14 10:08:07 -0400

    Card error fixed
    Changed "Cards[]" to "Card[]"

--- PizzaOrderingSystem/Pizza Ordering Application/PizzaOrderingSystem/Card.cs
+++ PizzaOrderingSystem/Pizza Ordering Application/PizzaOrderingSystem/Card.cs
@@ -19,7 +19,7 @@ namespace PizzaOrderingSystem {
 			get { return this.cardNickname; }
 			set {
                 //There's a compiler error here. Try to get it fixed as soon as possible.
-				Cards[] cards = owner.Cards;
+				Card[] cards = owner.Cards;
 				bool validName = true;
 				for (int i = 0; i < cards.Length; i++) {
 					if (value.Equals(cards[i].CardNickname)) {
