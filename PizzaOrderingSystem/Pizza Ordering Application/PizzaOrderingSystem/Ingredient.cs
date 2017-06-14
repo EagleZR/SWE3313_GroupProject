32c56fb0734a79616450a6b4dfe3d5869dfdc81b
Mark Zeagler <mark.zeagler@outlook.com>
2017-06-14 14:22:32 -0400

    Ingredient Refinement
    Added a new constructor, .Copy() method, and more thorough documentation
    for Ingredient

--- PizzaOrderingSystem/Pizza Ordering Application/PizzaOrderingSystem/Ingredient.cs
+++ PizzaOrderingSystem/Pizza Ordering Application/PizzaOrderingSystem/Ingredient.cs
@@ -2,6 +2,9 @@
 using PizzaOrderingSystem;
 
 namespace PizzaOrderingSystem {
+	/// <summary>
+	/// A class used to store information for each ingredient to be used on the pizza in the <see cref="PizzaOrderingSystem"/>.
+	/// </summary>
 	public class Ingredient {
 
 		#region Private Variables
@@ -32,41 +35,63 @@ namespace PizzaOrderingSystem {
 		#endregion
 
 		#region Constructors
-		public Ingredient ( string name, int category, int amount ) {
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="category">Use <see cref="PizzaOrderingSystem.Enums.IngredientCategory"/></param>
+		public Ingredient ( string name, int category ) {
 			this.Name = name;
 			this.Category = category;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="category">Use <see cref="PizzaOrderingSystem.Enums.IngredientCategory"/></param>
+		/// <param name="amount">Use <see cref="PizzaOrderingSystem.Enums.IngredientAmount"/></param>
+		public Ingredient ( string name, int category, int amount ) : this( name, category ) {
 			this.Amount = amount;
 		}
 		#endregion
 
 		#region Methods
 		/// <summary>
-		/// Increases the amount of this ingredient, not to exceed Enums.IngredientAmount.EXTRA
+		/// Increases the amount of this ingredient, not to exceed <see cref="Enums.IngredientAmount.EXTRA"/>.
 		/// </summary>
-		public void IncreaseAmount () {
+		protected void IncreaseAmount () {
 			if ( this.amount <= (int)Enums.IngredientAmount.EXTRA ) {
 				this.amount++;
 			}
 		}
 
 		/// <summary>
-		/// Decreases the amount of this ingredient, not to fall below Enums.IngredientAmount.NONE
+		/// Decreases the amount of this ingredient, not to fall below <see cref="Enums.IngredientAmount.NONE"/>.
 		/// </summary>
-		public void DecreaseAmount () {
+		protected void DecreaseAmount () {
 			if ( this.amount >= (int)Enums.IngredientAmount.NONE ) {
 				this.amount--;
 			}
 		}
 
-        /// <summary>
-        /// Sets the ingredient amount to the given value if the value does not exceed EXTRA or fall below NONE
-        /// </summary>
-        /// <param name="amount"></param>
-        public void SetAmount(int amount) {
-            if (amount <= (int)Enums.IngredientAmount.EXTRA && amount >= (int)Enums.IngredientAmount.NONE) {
-                this.amount = amount;
-            }
-        }
+		/// <summary>
+		/// Sets the ingredient amount to the given value if the value does not exceed <see cref="Enums.IngredientAmount.EXTRA"/> or fall below <see cref="Enums.IngredientAmount.NONE"/>
+		/// </summary>
+		/// <param name="amount">Use <see cref="Enums.IngredientAmount"/>.</param>
+		public void SetAmount ( int amount ) {
+			if ( amount <= (int)Enums.IngredientAmount.EXTRA && amount >= (int)Enums.IngredientAmount.NONE ) {
+				this.amount = amount;
+			}
+		}
+
+		/// <summary>
+		/// Returns a new copy of this <see cref="Ingredient"/>
+		/// </summary>
+		/// <returns></returns>
+		public Ingredient Copy() {
+			return new Ingredient( this.Name, this.Category, this.Amount );
+		}
 		#endregion
 	}
 }
