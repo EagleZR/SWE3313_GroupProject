4eb40dc503f7623b612651887cca2130a0f26d1a
Mark Zeagler <mark.zeagler@outlook.com>
2017-06-18 03:55:19 -0400

    And some more
    This was supposed to go with the last one... idk how it got deselected.
    I need some sleep :sleepy:

--- PizzaOrderingSystem/Pizza Ordering Application/PizzaOrderingSystem/Order.cs
+++ PizzaOrderingSystem/Pizza Ordering Application/PizzaOrderingSystem/Order.cs
@@ -5,7 +5,7 @@ namespace PizzaOrderingSystem {
 
 		#region Private Variables
 		private Customer purchaser;
-		private Pizza[] items;
+		private Pizza[] pizzas;
 		private int paymentType;
 		private Card paymentCard;
 		private int orderStatus = (int)Enums.OrderStatus.PENDING;
@@ -15,18 +15,18 @@ namespace PizzaOrderingSystem {
 		public Customer Purchaser {
 			get { return this.purchaser; }
 			set {
-				if ( this.OrderStatus == (int)Enums.OrderStatus.PENDING ) {
+				if( this.OrderStatus == (int)Enums.OrderStatus.PENDING ) {
 					this.purchaser = value;
 				}
 			}
 		}
-		public Pizza[] Items {
-			get { return this.items; }
+		public Pizza[] Pizzas {
+			get { return this.pizzas; }
 		}
 		public int PaymentType {
 			get { return this.paymentType; }
 			set {
-				if ( this.OrderStatus == (int)Enums.OrderStatus.PENDING ) {
+				if( this.OrderStatus == (int)Enums.OrderStatus.PENDING ) {
 					this.paymentType = value;
 				}
 			}
@@ -36,14 +36,14 @@ namespace PizzaOrderingSystem {
 		/// </summary>
 		public Card PaymentCard {
 			get {
-				if ( this.PaymentType == (int)Enums.PaymentType.CARD ) {
+				if( this.PaymentType == (int)Enums.PaymentType.CARD ) {
 					return this.paymentCard;
 				} else {
 					return null;
 				}
 			}
 			set {
-				if ( this.OrderStatus == (int)Enums.OrderStatus.PENDING && this.PaymentType == (int)Enums.PaymentType.CARD ) {
+				if( this.OrderStatus == (int)Enums.OrderStatus.PENDING && this.PaymentType == (int)Enums.PaymentType.CARD ) {
 					this.paymentCard = value;
 				}
 			}
@@ -61,7 +61,7 @@ namespace PizzaOrderingSystem {
 		/// Default/empty Order. Must be built for a customer. 
 		/// </summary>
 		/// <param name="purchaser"></param>
-		public Order ( Customer purchaser ) {
+		public Order( Customer purchaser ) {
 			this.purchaser = purchaser;
 			this.orderStatus = (int)Enums.OrderStatus.PENDING;
 		}
@@ -72,8 +72,8 @@ namespace PizzaOrderingSystem {
 		/// <param name="purchaser"></param>
 		/// <param name="items"></param>
 		/// <param name="paymentType"></param>
-		public Order ( Customer purchaser, Pizza[] items, int paymentType ) : this( purchaser ) {
-			this.items = items;
+		public Order( Customer purchaser, Pizza[] items, int paymentType ) : this( purchaser ) {
+			this.pizzas = items;
 			this.paymentType = paymentType;
 		}
 
@@ -84,7 +84,7 @@ namespace PizzaOrderingSystem {
 		/// <param name="items"></param>
 		/// <param name="paymentType"></param>
 		/// <param name="card"></param>
-		public Order ( Customer purchaser, Pizza[] items, int paymentType, Card card ) : this( purchaser, items, paymentType ) {
+		public Order( Customer purchaser, Pizza[] items, int paymentType, Card card ) : this( purchaser, items, paymentType ) {
 			this.PaymentCard = card;
 		}
 
@@ -96,25 +96,39 @@ namespace PizzaOrderingSystem {
 		/// <param name="paymentType"></param>
 		/// <param name="card"></param>
 		/// <param name="orderStatus"></param>
-		public Order ( Customer purchaser, Pizza[] items, int paymentType, Card card, int orderStatus ) : this( purchaser, items, paymentType ) {
+		public Order( Customer purchaser, Pizza[] items, int paymentType, Card card, int orderStatus ) : this( purchaser, items, paymentType ) {
 			this.orderStatus = orderStatus;
 		}
 		#endregion
 
 		#region Methods
-		public void AddItem ( Pizza item ) {
-			if (this.OrderStatus == (int)Enums.OrderStatus.PENDING) {
-				Pizza[] tempItems = items;
-				items = new Pizza[tempItems.Length + 1];
-				for (int i = 0; i < tempItems.Length; i++) {
-					items[i] = tempItems[i];
+		public void AddPizza( Pizza pizza ) {
+			if( this.OrderStatus == (int)Enums.OrderStatus.PENDING ) {
+				Pizza[] tempItems = pizzas;
+				pizzas = new Pizza[tempItems.Length + 1];
+				for( int i = 0; i < tempItems.Length; i++ ) {
+					pizzas[i] = tempItems[i];
 				}
-				items[items.Length - 1] = item;
+				pizzas[pizzas.Length - 1] = pizza;
 			} // TODO Throw exception?
 		}
 
+		public void RemovePizza( Pizza pizza ) {
+			if( this.OrderStatus == (int)Enums.OrderStatus.PENDING ) {
+				Pizza[] temp = new Pizza[pizzas.Length];
+				for( int i = 0, u = 0; i < pizzas.Length; i++, u++ ) {
+					if( pizzas[i].Equals( pizza ) ) {
+						u--;
+					} else {
+						temp[u] = pizzas[i];
+					}
+				}
+				pizzas = temp;
+			}
+		}
+
 		public void PlaceOrder() {
-			if ( this.OrderStatus == (int)Enums.OrderStatus.PENDING ) {
+			if( this.OrderStatus == (int)Enums.OrderStatus.PENDING ) {
 				this.orderStatus = (int)Enums.OrderStatus.SUBMITTED;
 				// TODO do more stuff
 			} // TODO Throw exception?
@@ -123,12 +137,22 @@ namespace PizzaOrderingSystem {
 		public string GenerateReceipt() {
 			string receipt = "Receipt:\n";
 			// TODO Build a receipt to be returned.
+			// 1. Generate "Store" info
+			receipt += "Thank you for your purchase!\n\nOrder Number: \n";
+
+			// 2. Generate Pizzas info
+
+			// 3. Generate Customer info
+
+			// 4. Generate Payment (card) info
+
+			// 5. Generate Tax, Tip, Total, Signature areas
 
 			return receipt;
 		}
 
 		public void FulfillOrder() {
-			if ( this.OrderStatus == (int)Enums.OrderStatus.SUBMITTED ) {
+			if( this.OrderStatus == (int)Enums.OrderStatus.SUBMITTED ) {
 				this.orderStatus = (int)Enums.OrderStatus.COMPLETED;
 				// TODO do more stuff
 			} // TODO Throw exception?
