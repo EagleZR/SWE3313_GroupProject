ddccd82ea1891f1eadbc33f0d7d42d57e51a3ea5
Mark Zeagler <mark.zeagler@outlook.com>
2017-06-20 13:50:14 -0400

    Customer second pass
    Added similar changes to the accessors to show an uninitialized value.
    Also changed the Add and Remove methods to not break when passed a null
    parameter

--- PizzaOrderingSystem/Pizza Ordering Application/PizzaOrderingSystem/Customer.cs
+++ PizzaOrderingSystem/Pizza Ordering Application/PizzaOrderingSystem/Customer.cs
@@ -16,17 +16,17 @@ namespace PizzaOrderingSystem {
 
 		#region Properties
 		public string Name {
-			get { return this.name; }
+			get { return ( this.name == null ? "name uninitialized" : this.name ); }
 			set { this.name = value; }
 		}
 
 		public string Address {
-			get { return this.address; }
+			get { return ( this.address == null ? "address uninitialized" : this.address ); }
 			set { this.address = value; }
 		}
 
 		public string PhoneNumber {
-			get { return this.phoneNumber; }
+			get { return ( this.phoneNumber == null ? "phone number initialized" : this.phoneNumber ); }
 			set { this.phoneNumber = value; }
 		}
 
@@ -46,7 +46,7 @@ namespace PizzaOrderingSystem {
 		/// <param name="name"></param>
 		/// <param name="address"></param>
 		/// <param name="phoneNumber"></param>
-		protected Customer ( string name, string address, string phoneNumber ) { 
+		protected Customer( string name, string address, string phoneNumber ) {
 			// TODO Do we even need this, or should we create a new customer in the database, then load back into here?
 			this.Name = name;
 			this.Address = address;
@@ -61,7 +61,7 @@ namespace PizzaOrderingSystem {
 		/// <param name="phoneNumber"></param>
 		/// <param name="pastOrders"></param>
 		/// <param name="cards"></param>
-		protected Customer ( string name, string address, string phoneNumber, Order[] pastOrders, Card[] cards ) : this( name, address, phoneNumber ) {
+		protected Customer( string name, string address, string phoneNumber, Order[] pastOrders, Card[] cards ) : this( name, address, phoneNumber ) {
 			this.pastOrders = pastOrders;
 			this.cards = cards;
 		}
@@ -79,42 +79,48 @@ namespace PizzaOrderingSystem {
 		/// Adds a new <see cref"PizzaOrderingSystem.Order"/> to the list of previous Orders.
 		/// </summary>
 		/// <param name="order">The new <see cref"PizzaOrderingSystem.Order"/> to be added.</param>
-		public void AddOrder ( Order order ) {
-			Order[] tempOrders = pastOrders;
-			pastOrders = new Order[tempOrders.Length + 1];
-			for ( int i = 0; i < tempOrders.Length; i++ ) {
-				pastOrders[i] = tempOrders[i];
+		public void AddOrder( Order order ) {
+			if( order != null ) {
+				Order[] tempOrders = pastOrders;
+				pastOrders = new Order[tempOrders.Length + 1];
+				for( int i = 0; i < tempOrders.Length; i++ ) {
+					pastOrders[i] = tempOrders[i];
+				}
+				pastOrders[pastOrders.Length - 1] = order;
 			}
-			pastOrders[pastOrders.Length - 1] = order;
 		}
 
 		/// <summary>
 		/// Adds a new <see cref"PizzaOrderingSystem.Card"/> to the list of available Cards.
 		/// </summary>
 		/// <param name="card">The new <see cref"PizzaOrderingSystem.Card"/> to be added.</param>
-		public void AddCard ( Card card ) {
-			Card[] tempCards = cards;
-			cards = new Card[tempCards.Length + 1];
-			for ( int i = 0; i < tempCards.Length; i++ ) {
-				cards[i] = tempCards[i];
+		public void AddCard( Card card ) {
+			if( card != null ) {
+				Card[] tempCards = cards;
+				cards = new Card[tempCards.Length + 1];
+				for( int i = 0; i < tempCards.Length; i++ ) {
+					cards[i] = tempCards[i];
+				}
+				cards[cards.Length - 1] = card;
 			}
-			cards[cards.Length - 1] = card;
 		}
 
 		/// <summary>
 		/// Removes the specified <see cref"PizzaOrderingSystem.Card"/> from the list of available Cards.
 		/// </summary>
 		/// <param name="card">The <see cref"PizzaOrderingSystem.Card"/> to be removed.</param>
-		public void RemoveCard ( Card card ) {
-			Card[] newCards = new Card[cards.Length - 1];
-			for ( int i = 0, u = 0; i < cards.Length; i++, u++ ) {
-				if ( cards[i] == card ) {
-					u--;
-				} else {
-					newCards[u] = cards[i];
+		public void RemoveCard( Card card ) {
+			if( card != null ) {
+				Card[] newCards = new Card[cards.Length - 1];
+				for( int i = 0, u = 0; i < cards.Length; i++, u++ ) {
+					if( cards[i] == card ) {
+						u--;
+					} else {
+						newCards[u] = cards[i];
+					}
 				}
+				cards = newCards;
 			}
-			cards = newCards;
 		}
 		#endregion
 	}
